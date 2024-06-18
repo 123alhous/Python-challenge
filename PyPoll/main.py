1eeefbbfe9047ca08bbcccb777fce09609ba0755
123alhous <palhousseiny@yahoo.fr>
2024-06-17 22:24:36 -0400

    Adding then main files and the results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,41 +1,41 @@
-# Import Libraries os and csv
 import os
 import csv
 
 election = os.path.join("C:/Users/admin/Desktop/Python-challenge/PyPoll/Resources/election_data.csv")
 
-
+# open and read the csv file
 with open(election, 'r') as file:
     data_election = csv.reader(file, delimiter=",")
 
-  # The total number of votes cast (row count after the header)
+  # The total number of votes cast
     next(data_election)
-    data = list(data_election)
-    row_count = len(data)
+    my_data = list(data_election)
+    row_count = len(my_data)
 
-  # Create new list from CSV column "C" to get a complete list of candidates who received votes
-    candilist = list()
-    tally = list()
+# A complete list of candidates who received votes
+    candi_list = list()
+    score = list()
     for i in range (0,row_count):
-        candidate = data[i][2]
-        tally.append(candidate)
-        if candidate not in candilist: 
-            candilist.append(candidate)
-    candicount = len(candilist)
+        candidate = my_data[i][2]
+        score.append(candidate)
+        if candidate not in candi_list: 
+            candi_list.append(candidate)
+    count = len(candi_list)
 
-  # The total number of votes each candidate won & the percentage of votes each candidate won
+  # The percentage and number of votes for each candidate 
     votes = list()
     percentage = list()
-    for j in range (0,candicount):
-        name = candilist[j]
-        votes.append(tally.count(name))
-        vprct = votes[j]/row_count
-        percentage.append(vprct)
+    for item in range (0,count):
+        names = candi_list[item]
+        votes.append(score.count(names))
+        value_p = votes[item]/row_count
+        percentage.append(value_p)
 
-  # The winner of the election based on popular vote.
-    winner = votes.index(max(votes))   
+ # The winner of the election based on popular vote
+    won = votes.index(max(votes))   
 
-  
+
+# export the results into the text file.  
 result = "C:/Users/admin/Desktop/Python-challenge/PyPoll/Analysis/election_result.txt"
 
 with open(result, 'w') as file:
@@ -44,8 +44,8 @@ with open(result, 'w') as file:
     file.write(f"Total Votes: {row_count}\n\n")
     file.write("----------------------------\n\n")
    
-    for k in range (0,candicount): 
-        file.write(f"{candilist[k]}: {percentage[k]:.3%} ({votes[k]})\n\n")
+    for k in range (0,count): 
+        file.write(f"{candi_list[k]}: {percentage[k]:.3%} ({votes[k]})\n\n")
     file.write("----------------------------\n\n")
-    file.write(f"Winner: {candilist[winner]}\n\n")
+    file.write(f"Winner: {candi_list[won]}\n\n")
     file.write("----------------------------\n")
